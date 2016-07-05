1f8892aa6c9e34168dd1a298ddc098cf1ed68ce9
ebattini <elizorel@msmu.edu>
2016-07-04 19:41:37 -0700

    asdf

---
+++ js/objectsdabble.js
@@ -0,0 +1,12 @@
+var myCat = {
+  name:'penny';
+  age:5;
+  isAlive:true;
+  color:'orange'
+  likesDog:true;
+
+}
+
+console.log('my cats name is ' + myCat.name + ' and she is ' + myCat.age + ' years old');
+
+myCat.color = 'black';
